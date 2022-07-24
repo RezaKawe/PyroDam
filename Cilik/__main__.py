48275c33aa83934646b8e123fb190c8e2c34d6e3
RezaKawe <109425207+RezaKawe@users.noreply.github.com>
2022-07-25 02:27:12 +0700

    Update __main__.py

--- Cilik/__main__.py
+++ Cilik/__main__.py
@@ -6,7 +6,7 @@
 # <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
 #
 # t.me/SharingUserbot & t.me/Lunatic0de
-# Dam-PyroBot
+# Rez-PyroBot
 
 from pyrogram import idle
 from uvloop import install
