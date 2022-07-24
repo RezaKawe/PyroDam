c85df86452e04f3bcb7edad3e1a4192999a054f7
RezaKawe <109425207+RezaKawe@users.noreply.github.com>
2022-07-25 02:28:57 +0700

    Update __init__.py

--- Cilik/__init__.py
+++ Cilik/__init__.py
@@ -6,7 +6,7 @@
 # <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
 #
 # t.me/SharingUserbot & t.me/Lunatic0de
-# Dam-PyroBot
+# Rez-PyroBot
 
 import asyncio
 import logging
